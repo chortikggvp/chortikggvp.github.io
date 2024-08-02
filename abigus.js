04dd8eade43cdaa5363cf04917c5d65da626e835
chortikggvp <matveyrodionov64@gmail.com>
2024-08-03 02:10:37 +0500

    Add files via upload

---
+++ abigus.js
@@ -0,0 +1,14 @@
+console.log("im coding rn")
+window.alert("WARNING UNDER CONSTRUCT")
+console.log("don't disturb me please im coding🦔")
+let x;
+let owe = `25 roubles`
+let no_one_asked;
+no_one_asked = window.prompt("...");
+window.alert(`ты сказал ${no_one_asked} ... но никто не ответил...`)
+
+
+document.getElementById("menu").onclick = function(){
+    window.alert("что ты хотел/а тут увидеть?")
+    
+}
