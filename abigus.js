5b68be9f3c7e80b2b91e8e85ff7188ff6678daad
chortikggvp <matveyrodionov64@gmail.com>
2024-08-08 21:26:00 +0500

    Update abigus.js

--- abigus.js
+++ abigus.js
@@ -16,7 +16,7 @@ document.getElementById("menu").onclick = function(){
         document.getElementById("amigo").style.visibility = "visible";
         document.getElementById("buttona").style.visibility = "visible";
         document.getElementById("theeline").style.visibility = "visible";
-        
+        document.getElementById("thingthatnobodycaresaboutlol").style.visibility = "visible";
         
 
     }
@@ -28,8 +28,12 @@ document.getElementById("menu").onclick = function(){
         document.getElementById("amigo").style.visibility = "hidden";
         document.getElementById("buttona").style.visibility = "hidden";
         document.getElementById("theeline").style.visibility = "hidden";
+        document.getElementById("thingthatnobodycaresaboutlol").style.visibility = "hidden";
         
         
     }
 
+}
+document.getElementById("whythefuckyouneedthisshitlol").onclick = function(){
+    window.alert("nigga why the fuck you need to report this website you think this is funny? i wasted 20 minutes customizing this shit and you want to just report it? fuck you nigga")
 }
