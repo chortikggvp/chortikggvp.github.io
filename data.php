6dc97808e44cfc79a174f292e9205eb300698eb7
chortikggvp <matveyrodionov64@gmail.com>
2024-08-30 14:35:49 +0500

    не сработало

--- data.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $db_server = "6.tcp.eu.ngrok.io:19006";
-    $db_user = "root";
-    $db_pass = "";
-    $db_name = "DB";
-    $con = "";
-    $con = mysqli_connect($db_server,$db_user,$db_pass,$db_name);
-    
-?>
